db84845553e6e5b9e4560de757ea242d859ab314
Adam Crews <adam@puppetlabs.com>
2013-12-12 00:51:09 -0800

    testing

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,15 +1,7 @@
 class phiera::mysql { 
 
-  $users = {
-    'hiera@%' => {
-      ensure        => present,
-      password_hash => mysql_password('suppersecret'),
-    }
-  }
-
   class { '::mysql::server':
     root_password => '1adam12',
-    #    users         => $users,
   }
 
   mysql::db { 'hiera':
